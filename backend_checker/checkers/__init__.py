69831589ba0c97cc518d5c0c49eb9b77f016d7b8
David Medioni <david.medioni@datapred.com>
2021-10-30 14:27:51 +0200

    Add basic framework for checkers

---
+++ backend_checker/checkers/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from abc import ABC, abstractmethod
+from typing import Dict, Sequence
+
+
+class Checker(ABC):
+    def run():
+        pass
+
+    def display(data: Sequence[Dict]) -> None:
+        pass
---
+++ backend_checker/checkers/connectors.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from furl.furl import furl
+from typing import Dict, Sequence
+from . import Checker
+
+
+class ConnectorsCheck(Checker):
+    def run():
+        pass
+
+    def display(data: Sequence[Dict]) -> None:
+        pass
